9c01d23912795cfd6af644101c6afa158b04fbe4
Sey Hong <kin19seyhong@gmail.com>
2023-08-12 15:09:03 +0700

    week_24_filter_exception

---
+++ src/main/java/com/infinite/solution/phoneshop/config/security/FilterChainExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.infinite.solution.phoneshop.config.security;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import ch.qos.logback.classic.Logger;
+import io.jsonwebtoken.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class FilterChainExceptionHandler extends OncePerRequestFilter {
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            log.error("Spring Security Filter Chain Exception:", e);
+            resolver.resolveException(request, response, null, e);
+        }
+    }
+}
--- src/main/java/com/infinite/solution/phoneshop/config/security/SecurityConfig.java
+++ src/main/java/com/infinite/solution/phoneshop/config/security/SecurityConfig.java
@@ -31,11 +31,15 @@ public class SecurityConfig{
 	
 	private AuthenticationConfiguration authenticationConfiguration;
 	
+	@Autowired
+	private FilterChainExceptionHandler filterChainExceptionHandler;
+	
 	
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 			.addFilter(new JwtAuthFilter(authenticationManager(authenticationConfiguration)))
+			.addFilterBefore(filterChainExceptionHandler, JwtAuthFilter.class)
 			.addFilterAfter(new TokenVerifyFilter(), JwtAuthFilter.class)
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
